a0c44a829cf3b1f64164fbb8380c64acd41db3ba
yoni-nachshon <76559424+yoni-nachshon@users.noreply.github.com>
2025-03-02 12:49:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+function loadResources(resources, isLibrary = true) {
+  resources?.forEach((src) => {
+      if (src.endsWith(".css") || src.endsWith(".scss")) {
+          if (!document.querySelector(`link[href="${src}"]`)) {
+              const link = document.createElement("link");
+              link.rel = "stylesheet";
+              link.href = src;
+              document.head.appendChild(link);
+          }
+      } else {
+          if (!document.querySelector(`script[src="${src}"]`)) {
+              const script = document.createElement("script");
+              if (!isLibrary) {
+                  script.setAttribute("data-plugins", "transform-modules-umd");
+                  script.type = "text/babel";
+                  script.setAttribute("data-type", "module");
+                  script.setAttribute("data-presets", "es2017,react,stage-3");
+              }
+              script.src = src;
+              document.body.appendChild(script);
+          }
+      }
+  });
+}
+
+const libraries = [
+  'react.development.js'
+  'react-dom.development.js'
+  'babel.min.js'
+  'pnp.js',
+  'react-bootstrap.min.js',
+  'react-bootstrap.min.css',
+  'icofont.min.css',
+  'jodit.fat.min.css',
+  'jodit.fat.min.js'
+];
+
+const form_files = [
+  "Editor.jsx",
+  "form.css",
+  "peoplePicker.jsx",
+  "utils.jsx",
+  "FormBuilder.jsx",
+  "Example.jsx",
+];
+
+(function () {
+  loadResources(libraries);
+  loadResources(form_files, false);
+})();
